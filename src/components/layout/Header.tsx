4e3ea184b4eb429026d4c94acd18249f7cc90273
Lokilife <bychkov-2007@inbox.ru>
2022-03-08 16:12:20 +0300

    Update: Footer & Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,22 @@
+import React, { Component } from 'react'
 import Image from 'next/image'
-import './Header.module.scss'
+import Styles from './Header.module.scss'
+import Link from 'next/link'
 
-function Header() {
-	return (
-		<div className="header">
-			<Image alt="Eclipse Avatar" src="/eclipse.svg" width="50" height="50" />
-			<p style={{fontSize: "15"}}>Eclipse</p>
-		</div>
-	)
+export default class Header extends Component {
+	render() {
+		return (
+			<div className={Styles.header}>
+				<div className='flex justify-center align-center m-left-50'>
+					<div className={Styles['header-logo-image']}>
+						<Image alt='Eclipse Avatar' width='100' height='100' objectFit='cover' src='/assets/images/logos/Eclipse.svg' />
+					</div>
+					<Link href='/'><a className='font-size-16 bold text-decoration-none'>Eclipse</a></Link>
+				</div>
+				<div className='m-right-50 font-size-15'>
+					Log In
+				</div>
+			</div>
+		)
+	}
 }
-
-export default Header
